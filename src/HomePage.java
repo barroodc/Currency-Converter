e55b5a72671dfe578d16c35964efe3fcb827abef
barroodc <53627915+barroodc@users.noreply.github.com>
2021-08-31 22:59:46 -0400

    Added textfields

--- src/HomePage.java
+++ src/HomePage.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class HomePage {
+public class HomePage implements ActionListener {
 private int clicks = 0;
 private JLabel label = new JLabel("Number of clicks: 0 ");
 private JFrame frame = new JFrame();
@@ -12,13 +12,22 @@ public HomePage(){
 
     JButton button = new JButton("Conversion");
     JButton button2 = new JButton("Live Exchange Rates");
-    button.addActionListener(this::actionPerformed);
+    JButton button3 = new JButton("Historical Exchange Rates");
+    button.addActionListener(this);
+
+    JTextField dollarAmount = new JTextField("");
+    JTextField countryFrom = new JTextField("");
+    JTextField countryTo = new JTextField("");
 
     JPanel panel = new JPanel();
-    panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+    panel.setBorder(BorderFactory.createEmptyBorder(220, 320, 200, 350));
     panel.setLayout(new GridLayout(0, 1));
     panel.add(button);
     panel.add(button2);
+    panel.add(button3);
+    panel.add(dollarAmount);
+    panel.add(countryFrom);
+    panel.add(countryTo);
     panel.add(label);
 
     frame.add(panel, BorderLayout.CENTER);
@@ -26,6 +35,7 @@ public HomePage(){
     frame.setTitle("Currency Converter");
     frame.pack();
     frame.setVisible(true);
+
 }
 
 public void actionPerformed(ActionEvent e) {
