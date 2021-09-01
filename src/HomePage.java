5fae6966bf51f316dcfb49eb6e3aafd3bfbb11d7
barroodc <53627915+barroodc@users.noreply.github.com>
2021-09-01 18:31:59 -0400

    Was able to add a switch logo and still working with different design ideas for home page

--- src/HomePage.java
+++ src/HomePage.java
@@ -3,44 +3,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class HomePage implements ActionListener {
-private int clicks = 0;
-private JLabel label = new JLabel("Number of clicks: 0 ");
-private JFrame frame = new JFrame();
+public class HomePage {
 
 public HomePage(){
 
-    JButton button = new JButton("Conversion");
-    JButton button2 = new JButton("Live Exchange Rates");
-    JButton button3 = new JButton("Historical Exchange Rates");
-    button.addActionListener(this);
+    JFrame f = new JFrame("Hello");
 
-    JTextField dollarAmount = new JTextField("");
-    JTextField countryFrom = new JTextField("");
-    JTextField countryTo = new JTextField("");
+    JPanel MyPanel = new JPanel();
 
-    JPanel panel = new JPanel();
-    panel.setBorder(BorderFactory.createEmptyBorder(220, 320, 200, 350));
-    panel.setLayout(new GridLayout(0, 1));
-    panel.add(button);
-    panel.add(button2);
-    panel.add(button3);
-    panel.add(dollarAmount);
-    panel.add(countryFrom);
-    panel.add(countryTo);
-    panel.add(label);
+    MyPanel.setLayout( new FlowLayout() );
 
-    frame.add(panel, BorderLayout.CENTER);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setTitle("Currency Converter");
-    frame.pack();
-    frame.setVisible(true);
+    JButton liveExchangeRate = new JButton("Live Exchange Rate");
+    JButton historicalExchangeRate = new JButton("Historical Exchange Rate");
+    Icon icon = new ImageIcon("/Users/christopher/Desktop/switch1.png");
+    JButton flipConversions = new JButton(icon);
 
-}
+    JButton convert = new JButton("Convert");
+    MyPanel.add(liveExchangeRate);
+    MyPanel.add(historicalExchangeRate);
+    MyPanel.add(flipConversions);
+    MyPanel.add(convert);
 
-public void actionPerformed(ActionEvent e) {
-    clicks++;
-    label.setText("Number of clicks: " + clicks);
+    f.getContentPane().add(MyPanel, "Center");
+    f.setSize(300, 300);
+    f.setVisible(true);
+
 }
 
 public static void main(String[] args){
---
+++ src/Test.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Test {
+
+    public static void main(String[] args)
+    {
+        JFrame f = new JFrame("Hello");
+
+        JPanel MyPanel = new JPanel();
+
+        MyPanel.setLayout( new FlowLayout() );    // Demo flow layout
+
+        JButton x1 = new JButton("I am x1");
+        JButton x2 = new JButton("I am x2");
+        JButton x3 = new JButton("I am x3");
+        JButton x4 = new JButton("I am x4");
+        JButton x5 = new JButton("I am x5");
+
+        MyPanel.add(x1);           // Add JButtons using Flow Layout
+        MyPanel.add(x2);
+        MyPanel.add(x3);
+        MyPanel.add(x4);
+        MyPanel.add(x5);
+
+        f.getContentPane().add(MyPanel, "Center"); // Paste MyPanel in center
+        // of the contentPane
+        f.setSize(300, 300);
+        f.setVisible(true);
+    }
+
+}
