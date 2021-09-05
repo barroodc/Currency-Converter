b636c607f43edc835b88e4f4e65acb38c3e9892c
barroodc <53627915+barroodc@users.noreply.github.com>
2021-09-05 12:55:54 -0400

    Was able to appropriately align everything. Now I can start working on other classes.

--- src/Test.java
+++ src/Test.java
@@ -17,7 +17,7 @@ public class Test {
         button.setBounds(150, 180, size.width, size.height);
         Dimension size2 = button2.getPreferredSize();
         button2.setBounds(400, 180, size2.width, size2.height);
-        button3.setBounds(600, 310, 50, 50);
+        button3.setBounds(620, 310, 50, 50);
         JButton button4 = new JButton("Convert");
         Dimension size4 = button4.getPreferredSize();
         button4.setBounds(1000, 500, size4.width, size4.height);
@@ -34,10 +34,28 @@ public class Test {
         frame.setSize(1200, 600);
         frame.setVisible(true);
 
+
         JLabel amount = new JLabel("Amount");
-        amount.setBounds(50,50,200,30);
+        amount.setBounds(150,275,200,30);
+        Font labelFont = amount.getFont();
+        amount.setFont(new Font(labelFont.getName(), Font.PLAIN, 20));
+
+        JLabel from = new JLabel("From");
+        from.setBounds(400,275,200,30);
+        Font labelFont2 = from.getFont();
+        from.setFont(new Font(labelFont2.getName(), Font.PLAIN, 20));
+
+        JLabel to = new JLabel("To");
+        to.setBounds(715,275,200,30);
+        Font labelFont3 = to.getFont();
+        to.setFont(new Font(labelFont3.getName(), Font.PLAIN, 20));
+
         frame.add(amount);
         panel.add(amount);
+        frame.add(from);
+        panel.add(from);
+        frame.add(to);
+        panel.add(to);
 
         JTextField motto1 = new JTextField();
         motto1.setFont(motto1.getFont().deriveFont(Font.PLAIN,20));
@@ -45,10 +63,11 @@ public class Test {
         frame.add(motto1);
         panel.add(motto1);
 
+
         String countryFrom[] = {"India", "Aus", "U.S.A", "England", "Newzealand"};
         JComboBox cb = new JComboBox(countryFrom);
         cb.setFont(cb.getFont().deriveFont(Font.PLAIN, 20));
-        cb.setBounds(450, 325, 150, 20);
+        cb.setBounds(400, 325, 180, 20);
         frame.add(cb);
         frame.setLayout(null);
         frame.setSize(400, 500);
@@ -59,7 +78,7 @@ public class Test {
         String countryTo[] = {"India", "Aus", "U.S.A", "England", "Newzealand"};
         JComboBox cb2 = new JComboBox(countryTo);
         cb2.setFont(cb.getFont().deriveFont(Font.PLAIN, 20));
-        cb2.setBounds(675, 325, 180, 20);
+        cb2.setBounds(715, 325, 180, 20);
         frame.add(cb2);
         frame.setLayout(null);
         frame.setSize(400, 500);
