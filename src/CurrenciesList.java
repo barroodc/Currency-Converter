7fe64eeafa43761e1777f0b163300261a96768a3
barroodc <53627915+barroodc@users.noreply.github.com>
2021-09-06 13:37:08 -0400

    Have to figure out how to neatly and clearly write the CurrencyList class so that I can use it throughout the entire program.

--- src/CurrenciesList.java
+++ src/CurrenciesList.java
@@ -1,2 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CurrenciesList {
+
+   public static void Currency() throws FileNotFoundException {
+       File file = new File("/Users/christopher/Desktop/AlphaCurrencyList.txt");
+       Scanner sc = new Scanner(file);
+
+       ArrayList<String> alphaList = new ArrayList<>();
+
+       while (sc.hasNext()){
+           alphaList.add(sc.nextLine());
+       }
+       String[] finalAlphaList = alphaList.toArray(new String[alphaList.size()]);
+
+
+   }
+
+    public static void main(String[] args) throws FileNotFoundException {
+       Currency();
+    }
 }
--- src/HomePage.java
+++ src/HomePage.java
@@ -2,36 +2,94 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.FileNotFoundException;
 
 public class HomePage {
 
-public HomePage(){
 
-    JFrame f = new JFrame("Currency Converter");
+        public static void main(String[] args) throws FileNotFoundException {
+        JFrame frame = new JFrame("Currency Converter");
+        JPanel panel = new JPanel();
+        frame.getContentPane();
+        JButton button = new JButton("Historical Exchange Rates");
+        JButton button2 = new JButton("Live Exchange Rates");
+        Dimension size = button.getPreferredSize();
+        Icon icon = new ImageIcon("/Users/christopher/Desktop/switch1.png");
+        JButton button3 = new JButton(icon);
+        button.setBounds(150, 180, size.width, size.height);
+        Dimension size2 = button2.getPreferredSize();
+        button2.setBounds(400, 180, size2.width, size2.height);
+        button3.setBounds(620, 310, 50, 50);
+        JButton button4 = new JButton("Convert");
+        Dimension size4 = button4.getPreferredSize();
+        button4.setBounds(1000, 500, size4.width, size4.height);
 
-    JPanel MyPanel = new JPanel();
 
-    MyPanel.setLayout( new FlowLayout() );
+        panel.setLayout(null);
+        panel.add(button);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(button4);
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.setSize(1200, 600);
+        frame.setVisible(true);
 
-    JButton liveExchangeRate = new JButton("Live Exchange Rate");
-    JButton historicalExchangeRate = new JButton("Historical Exchange Rate");
-    Icon icon = new ImageIcon("/Users/christopher/Desktop/switch1.png");
-    JButton flipConversions = new JButton(icon);
-    JButton convert = new JButton("Convert");
-    MyPanel.add(liveExchangeRate);
-    MyPanel.add(historicalExchangeRate);
-    MyPanel.add(flipConversions);
-    MyPanel.add(convert);
 
-    //f.getContentPane().add(MyPanel, "Center");
-    f.setSize(1200,600);
-    f.setVisible(true);
+        JLabel amount = new JLabel("Amount");
+        amount.setBounds(150, 275, 200, 30);
+        Font labelFont = amount.getFont();
+        amount.setFont(new Font(labelFont.getName(), Font.PLAIN, 20));
+
+        JLabel from = new JLabel("From");
+        from.setBounds(400, 275, 200, 30);
+        Font labelFont2 = from.getFont();
+        from.setFont(new Font(labelFont2.getName(), Font.PLAIN, 20));
+
+        JLabel to = new JLabel("To");
+        to.setBounds(715, 275, 200, 30);
+        Font labelFont3 = to.getFont();
+        to.setFont(new Font(labelFont3.getName(), Font.PLAIN, 20));
+
+        frame.add(amount);
+        panel.add(amount);
+        frame.add(from);
+        panel.add(from);
+        frame.add(to);
+        panel.add(to);
+
+        JTextField motto1 = new JTextField();
+        motto1.setFont(motto1.getFont().deriveFont(Font.PLAIN, 20));
+        motto1.setBounds(150, 325, 200, 30);
+        frame.add(motto1);
+        panel.add(motto1);
+
+
+        JComboBox cb = new JComboBox();
+        cb.setFont(cb.getFont().deriveFont(Font.PLAIN, 20));
+        cb.setBounds(400, 325, 180, 20);
+        frame.add(cb);
+        frame.setLayout(null);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+        panel.add(cb);
+
+
+        String countryTo[] = {"India", "Aus", "U.S.A", "England", "Newzealand"};
+        JComboBox cb2 = new JComboBox(countryTo);
+        cb2.setFont(cb.getFont().deriveFont(Font.PLAIN, 20));
+        cb2.setBounds(715, 325, 180, 20);
+        frame.add(cb2);
+        frame.setLayout(null);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+        panel.add(cb2);
+
+
+   }
+
 
 }
 
-public static void main(String[] args){
-    new HomePage();
 
-}
-
-}
