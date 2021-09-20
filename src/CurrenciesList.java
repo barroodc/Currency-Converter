e4e68f1e11ae9e5ae9c4eb7c4165e36d939909b9
barroodc <53627915+barroodc@users.noreply.github.com>
2021-09-19 23:14:56 -0400

    Adding everything manually now. No longer using text file for the time being.

--- src/CurrenciesList.java
+++ src/CurrenciesList.java
@@ -1,61 +1,46 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 
-public class CurrenciesList extends JFrame {
+public class CurrenciesList {
 
-    private JComboBox box;
-    private JLabel picture;
-
-    InputStream file = ClassLoader.class.getResourceAsStream("resources/chart.png");
-
-    private static String[] filename = {"/Users/christopher/Desktop/Abkhazian.png"};
-    private Icon[] pics = {new ImageIcon(getClass().getResource(filename[0]))};
-
-    public CurrenciesList(){
-        super("Currency Converter");
-        setLayout(new FlowLayout());
-
-        box = new JComboBox(filename);
-
-        box.addItemListener(
-                new ItemListener() {
-                    public void itemStateChanged(ItemEvent event){
-                        if (event.getStateChange() == ItemEvent.SELECTED)
-                            picture.setIcon(pics[box.getSelectedIndex()]);
-                    }
-                }
-        );
-
-        add(box);
-        picture = new JLabel(pics[0]);
-        add(picture);
-    }
-
-    public static void main(String[] args){
-        CurrenciesList go = new CurrenciesList();
-        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        go.setSize(300,200);
-        go.setVisible(true);
-    }
-
-}
-
-
-class Flags {
 
 
     public static class CustomComboBoxDemo extends JPanel {
         ImageIcon[] images;
-        String[] countries = {"USA", "Canada", "Montreal", "Iran", "Lebanon"};
+        String s = "AbkhazianApsar(ABK)\n" +
+                "AfghanistanAfghani(AFN)\n" +
+                "AlbanianLek(ALL)\n" +
+                "AlderneyPound(none)\n" +
+                "AlgerianDinar(DZD)\n" +
+                "AndorranFranc(ADF)\n" +
+                "AndorranPeseta(ADP)\n" +
+                "AngolanKwanza(AOA)\n" +
+                "AngolanOldKwanza(AON)\n" +
+                "ArgentinePeso(ARS)\n" +
+                "ArmenianDram(AMD)\n" +
+                "ArtsakhDram(none)\n" +
+                "ArubanFlorin(AWG)\n" +
+                "AustralianDollar(AUD)\n" +
+                "AustrianSchilling(ATS)\n" +
+                "AzerbaijaninewManat(AZN)\n" +
+                "AzerbaijanOldManat(AZM)\n" +
+                "BahamianDollar(BSD)\n" +
+                "BahrainiDinar(BHD)\n" +
+                "BangladeshiTaka(BDT)\n";
+        String[] countries = s.split("\\s");
 
-        public CustomComboBoxDemo() {
-            super(new BorderLayout());
+
+
+        /*
+         * Despite its use of EmptyBorder, this panel makes a fine content
+         * pane because the empty border just increases the panel's size
+         * and is "painted" on top of the panel's normal background.  In
+         * other words, the JPanel fills its entire background if it's
+         * opaque (which it is by default); adding a border doesn't change
+         * that.
+         */
+        public CustomComboBoxDemo() throws IOException {
 
             //Load the pet images and create an array of indexes.
             images = new ImageIcon[countries.length];
@@ -68,14 +53,19 @@ class Flags {
                 }
             }
 
-            JComboBox petList = new JComboBox(intArray);
+
+            //Create the combo box.
+            JComboBox countriesList = new JComboBox(intArray);
+            countriesList.setFont(countriesList.getFont().deriveFont(Font.PLAIN, 20));
+            countriesList.setBounds(300, 325, 2000, 20);
             ComboBoxRenderer renderer= new ComboBoxRenderer();
             renderer.setPreferredSize(new Dimension(200, 130));
-            petList.setRenderer(renderer);
-            petList.setMaximumRowCount(3);
+            countriesList.setRenderer(renderer);
+            countriesList.setMaximumRowCount(3);
 
 
-            add(petList, BorderLayout.PAGE_START);
+            //Lay out the demo.
+            add(countriesList, BorderLayout.PAGE_START);
             setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
 
         }
@@ -91,24 +81,40 @@ class Flags {
             }
         }
 
-        private static void createAndShowGUI() {
+        /**
+         * Create the GUI and show it.  For thread safety,
+         * this method should be invoked from the
+         * event-dispatching thread.
+         */
+        private static void createAndShowGUI() throws IOException {
+            //Make sure we have nice window decorations.
             JFrame.setDefaultLookAndFeelDecorated(true);
 
-            JFrame frame = new JFrame("CustomComboBoxDemo");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            //Create and set up the window.
+            JFrame frame = new JFrame("Currency Converter");
+            JButton button = new JButton();
+            //frame.setVisible(true);
 
+            //Create and set up the content pane.
             JComponent newContentPane = new CustomComboBoxDemo();
-            newContentPane.setOpaque(true);
+            newContentPane.setOpaque(true); //content panes must be opaque
             frame.setContentPane(newContentPane);
 
+            //Display the window.
             frame.pack();
             frame.setVisible(true);
         }
 
         public static void main(String[] args) {
+            //Schedule a job for the event-dispatching thread:
+            //creating and showing this application's GUI.
             javax.swing.SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
-                    createAndShowGUI();
+                    try {
+                        createAndShowGUI();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             });
         }
@@ -123,13 +129,21 @@ class Flags {
                 setVerticalAlignment(CENTER);
             }
 
+
+
+            /*
+             * This method finds the image and text corresponding
+             * to the selected value and returns the label, set up
+             * to display the text and image.
+             */
             public Component getListCellRendererComponent(
                     JList list,
                     Object value,
                     int index,
                     boolean isSelected,
                     boolean cellHasFocus) {
-
+                //Get the selected index. (The index param isn't
+                //always valid, so just use the value.)
                 int selectedIndex = ((Integer)value).intValue();
 
                 if (isSelected) {
@@ -140,7 +154,7 @@ class Flags {
                     setForeground(list.getForeground());
                 }
 
-
+                //Set the icon and text.  If icon was null, say so.
                 ImageIcon icon = images[selectedIndex];
                 String pet = countries[selectedIndex];
                 setIcon(icon);
@@ -155,6 +169,7 @@ class Flags {
                 return this;
             }
 
+            //Set the font and text when no image was found.
             protected void setUhOhText(String uhOhText, Font normalFont) {
                 if (uhOhFont == null) { //lazily create this font
                     uhOhFont = normalFont.deriveFont(Font.ITALIC);
@@ -165,4 +180,3 @@ class Flags {
         }
     }
 }
-
